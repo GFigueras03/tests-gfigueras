2d70f86e0dc0b953934564ac0ed89091fa4d2df2
GFigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2022-12-13 21:30:14 +0100

    add test

---
+++ src/main/java/daw/programacion/calculadora/Calculadora.java
@@ -0,0 +1,8 @@
+package daw.programacion.calculadora;
+
+public class Calculadora {
+
+    public int sumar(int numero1, int numero2) {
+        return numero1 + numero1;
+    }
+}
--- src/test/java/daw/programacion/calculadora/CalculadoraTest.java
+++ src/test/java/daw/programacion/calculadora/CalculadoraTest.java
@@ -1,8 +1,15 @@
 package daw.programacion.calculadora;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalculadoraTest {
+    @Test
+    public void dosSumarDosEsCuatro() {
+        int expected = 4;
+        int numero1 = 1, numero2 = 2;
+        Calculadora calc = new Calculadora();
 
+        assertEquals(expected, calc.sumar(numero1, numero2));
+    }
 }
