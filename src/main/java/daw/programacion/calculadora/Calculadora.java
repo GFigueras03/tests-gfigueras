748cdb5a80f66cd8de4fbc9c5d47e3a1e2f7b5b0
GFigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2022-12-13 21:43:44 +0100

    add 5sumar7es12

--- src/main/java/daw/programacion/calculadora/Calculadora.java
+++ src/main/java/daw/programacion/calculadora/Calculadora.java
@@ -3,6 +3,6 @@ package daw.programacion.calculadora;
 public class Calculadora {
 
     public int sumar(int numero1, int numero2) {
-        return numero1 + numero1;
+        return numero1 * numero2;
     }
 }
